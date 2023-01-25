nspcc-dev/neofs-node

[#2176] neofs-node: Do not invalidate old cache items
Signed-off-by: Evgenii Stratonikov <e.stratonikov@yadro.com>

cmd/neofs-node/cache.go
@@ -321,9 +321,14 @@ func (s *ttlContainerLister) update(owner user.ID, cnr cid.ID, add bool) {
 		return
 	}
 
+	vt := val.(*valueWithTime)
+	if s.ttl <= time.Since(vt.t) {
+		return
+	}
+
 	// panic on typecast below is OK since developer must be careful,
 	// runtime can do nothing with wrong type occurrence
-	item := val.(*valueWithTime).v.(*cacheItemContainerList)
+	item := vt.v.(*cacheItemContainerList)
 
 	item.mtx.Lock()
 	{

TrueCloudLab/frostfs-node

cmd/frostfs-node/cache.go
@@ -294,6 +294,10 @@ func (s *ttlContainerLister) update(owner user.ID, cnr cid.ID, add bool) {
 		return
 	}
 
+	if s.ttl <= time.Since(val.t) {
+		return
+	}
+
 	item := val.v
 
 	item.mtx.Lock()